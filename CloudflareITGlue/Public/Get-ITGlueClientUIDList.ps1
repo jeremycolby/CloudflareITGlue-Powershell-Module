e077a715b2b93e91dec7850edfff03c7bb515af3
jeremycolby <43423017+jeremycolby@users.noreply.github.com>
2018-11-08 12:51:57 -0800

    Changed UID format for easier sorting/list reading

--- CloudflareITGlue/Public/Get-ITGlueClientUIDList.ps1
+++ CloudflareITGlue/Public/Get-ITGlueClientUIDList.ps1
@@ -1,18 +1,19 @@
 function Get-ITGlueClientUIDList {
     $UIDList = @()
     $Progress = 0
+    
     $Clients = New-ITGlueWebRequest -Endpoint 'organizations' -Method 'GET' | ForEach-Object data | Where-Object {
         $_.attributes.'organization-type-name' -eq 'Client'}
 
     foreach ($Client in $Clients) {
         Write-Progress -Activity 'ITGlueAPI' -Status 'Creating Client UID List' -CurrentOperation $Client.attributes.name -PercentComplete ($Progress / ($Clients | Measure-Object | ForEach-Object count) * 100)
-        $UIDList += $Client.id + '__' + $Client.attributes.name
+        $UIDList += $Client.attributes.name + '__' + $Client.id
         $Progress++
     }
     if($UIDList){
         Write-Progress -Activity 'ITGlueAPI' -Status 'Complete' -PercentComplete 100
         try {
-            $UIDList | Out-File 'ITGlueClientUIDList.txt'
+            $UIDList | Sort-Object | Out-File 'ITGlueClientUIDList.txt'
             Invoke-Item 'ITGlueClientUIDList.txt'
         }
         catch {
