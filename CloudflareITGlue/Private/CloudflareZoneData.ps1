efdaf3622db8b7b7aa86dfe22bdc94483d2e1e6d
Jeremy Colby <jcolby@yournucleus.ca>
2018-11-13 14:12:17 -0800

    Add ITGOrgID to Zone Data array & cleanup

--- CloudflareITGlue/Private/CloudflareZoneData.ps1
+++ CloudflareITGlue/Private/CloudflareZoneData.ps1
@@ -1,103 +1,90 @@
 function Get-CloudflareZoneData {
     param(
-        [Parameter(Mandatory = $true)][string]$ZoneId
+        [Parameter(Mandatory = $true)][string]$ZoneId,
+        [Parameter(Mandatory = $true)][pscustomobject]$ITGDomains
     )
-
-    $ZoneRecords = New-CloudflareWebRequest -Endpoint "zones/$ZoneId/dns_records"
-    $RecordArray = @()
-    $ITGlueClientUIDRecord = $null
     
-    foreach ($Record in $ZoneRecords.result) {
-        $RecordEntry = [ordered]@{
-            Type     = $Record.type
-            Name     = $Record.name
-            Value    = $Record.content
-            Priority = $Record.priority
-            TTL      = if ($Record.ttl -eq 1){'Auto'}else{$Record.ttl}
-            Proxied  = $Record.proxied
-            Modified = ($Record.modified_on.Replace('T', ' ') -split '\.')[0]
-        }
-        $RecordArray += New-Object PSObject -Property $RecordEntry
-        
-        if ($Record.name -eq "itglueclientuid.$($Record.zone_name)") {
-            $ITGlueClientUIDRecord = $Record.content
+    $Timestamp = (Get-Date).ToUniversalTime().ToString("yyyy-M-d HH:mm:ss")
+    $ZoneInfo = New-CloudflareWebRequest -Endpoint "zones/$ZoneId"
+    $ZoneRecords = New-CloudflareWebRequest -Endpoint "zones/$ZoneId/dns_records"
+    $ZoneFileData = New-CloudflareWebRequest -Endpoint "zones/$ZoneId/dns_records/export"
+    $MatchId = $null
+    foreach($ITGDomain in $ITGDomains){
+        if($ZoneInfo.result.name.tolower() -eq $ITGDomain.name.tolower()){
+            $MatchId = $ITGDomain.'organization-id'
         }
     }
-
-    if ($ITGlueClientUIDRecord) {
-        $ZoneInfo = New-CloudflareWebRequest -Endpoint "zones/$ZoneId"
-        $Timestamp = (Get-Date).ToUniversalTime().ToString("yyyy-M-d HH:mm:ss")
-        $ZoneFileData = New-CloudflareWebRequest -Endpoint "zones/$ZoneId/dns_records/export"
-        $ZoneFileData = $ZoneFileData.Replace(
-            "@	3600	IN	SOA	$($ZoneInfo.result.name).	root.$($ZoneInfo.result.name).	(",
-            "@	3600	IN	SOA	$($ZoneInfo.result.name_servers[0]). $((($CloudflareAPIEmail -split '@')[0]).Replace('.','\.') + '.'+ ($CloudflareAPIEmail -split '@')[1]). ("
-        )
-        $ZoneFileData = $ZoneFileData.Replace(
-            ";; NS Records (YOU MUST CHANGE THIS)`n$($ZoneInfo.result.name).	1	IN	NS	" + 'REPLACE&ME$WITH^YOUR@NAMESERVER.',
-            ";; NS Records`n$($ZoneInfo.result.name).	1	IN	NS	$($ZoneInfo.result.name_servers[0]).`n$($ZoneInfo.result.name).	1	IN	NS	$($ZoneInfo.result.name_servers[1])."
-        )
-        $ZoneFileData = $ZoneFileData.Replace(
-            ';;   -- update the SOA record with the correct authoritative name server',
-            ";;   -- update the SOA record with the correct authoritative name server`n;;   ** CloudflareITGlue Module: Updated $($Timestamp)"
-        )
-        $ZoneFileData = $ZoneFileData.Replace(
-            ';;   -- update the SOA record with the contact e-mail address information',
-            ";;   -- update the SOA record with the contact e-mail address information`n;;   ** CloudflareITGlue Module: Updated $($Timestamp)"
-        )
-        $ZoneFileData = $ZoneFileData.Replace(
-            ';;   -- update the NS record(s) with the authoritative name servers for this domain.',
-            ";;   -- update the NS record(s) with the authoritative name servers for this domain.`n;;   ** CloudflareITGlue Module: Updated $($Timestamp)"
-        )
-        $RecordsHtml = 
-        '<div>
-            <table id="RecordTable" style="width:100%">
-                <thead>
-                    <th>Type</th>
-                    <th>Name</th>
-                    <th>Value</th>
-                    <th>Priority</th>
-                    <th>TTL</th>
-                    <th>Proxied</th>
-                    <th>Modified</th>
-                </thead>
-                <tbody>' +
-                $(foreach ($Record in $RecordArray) {
-                    "<tr>
-                        <td>$($Record.type)</td>
-                        <td>$($Record.name)</td>
-                        <td>$($Record.value)</td>
-                        <td>$($Record.priority)</td>
-                        <td>$($Record.ttl)</td>
-                        <td>$($Record.proxied)</td>
-                        <td>$($Record.modified)</td>
-                    </tr>"
-                }) +
-                '</tbody>
-            </table>
-        </div>'
-        
-        $ZoneData = [ordered]@{
-            Name           = $ZoneInfo.result.name
-            ITGlueClientID = ($ITGlueClientUIDRecord -split '__')[1]
-            SyncDate       = $Timestamp
-            CfNameServers  = $ZoneInfo.result.name_servers
-            Status         = $ZoneInfo.result.status
-            ZoneFileData   = $ZoneFileData
-            DNSRecords     = $RecordArray
-            RecordsHtml    = $RecordsHtml
-        }
-        $ZoneData
+    
+    $ZoneFileData = $ZoneFileData.Replace(
+        "@	3600	IN	SOA	$($ZoneInfo.result.name).	root.$($ZoneInfo.result.name).	(",
+        "@	3600	IN	SOA	$($ZoneInfo.result.name_servers[0]). $((($CloudflareAPIEmail -split '@')[0]).Replace('.','\.') + '.'+ ($CloudflareAPIEmail -split '@')[1]). ("
+    )
+    $ZoneFileData = $ZoneFileData.Replace(
+        ";; NS Records (YOU MUST CHANGE THIS)`n$($ZoneInfo.result.name).	1	IN	NS	" + 'REPLACE&ME$WITH^YOUR@NAMESERVER.',
+        ";; NS Records`n$($ZoneInfo.result.name).	1	IN	NS	$($ZoneInfo.result.name_servers[0]).`n$($ZoneInfo.result.name).	1	IN	NS	$($ZoneInfo.result.name_servers[1])."
+    )
+    $ZoneFileData = $ZoneFileData.Replace(
+        ';;   -- update the SOA record with the correct authoritative name server',
+        ";;   -- update the SOA record with the correct authoritative name server`n;;   ** CloudflareITGlue Module: Updated $($Timestamp)"
+    )
+    $ZoneFileData = $ZoneFileData.Replace(
+        ';;   -- update the SOA record with the contact e-mail address information',
+        ";;   -- update the SOA record with the contact e-mail address information`n;;   ** CloudflareITGlue Module: Updated $($Timestamp)"
+    )
+    $ZoneFileData = $ZoneFileData.Replace(
+        ';;   -- update the NS record(s) with the authoritative name servers for this domain.',
+        ";;   -- update the NS record(s) with the authoritative name servers for this domain.`n;;   ** CloudflareITGlue Module: Updated $($Timestamp)"
+    )
+    $RecordsHtml = 
+    '<div>
+        <table id="RecordTable" style="width:100%">
+            <thead>
+                <th>Type</th>
+                <th>Name</th>
+                <th>Value</th>
+                <th>Priority</th>
+                <th>TTL</th>
+                <th>Proxied</th>
+                <th>Modified</th>
+            </thead>
+            <tbody>' +
+            $(foreach ($Record in $ZoneRecords.result) {
+                "<tr>
+                    <td>$($Record.type)</td>
+                    <td>$($Record.name)</td>
+                    <td>$($Record.content)</td>
+                    <td>$($Record.priority)</td>
+                    <td>$(if ($Record.ttl -eq 1){'Auto'}else{$Record.ttl})</td>
+                    <td>$($Record.proxied)</td>
+                    <td>$(($Record.modified_on.Replace('T', ' ') -split '\.')[0])</td>
+                </tr>"
+            }) +
+            '</tbody>
+        </table>
+    </div>'
+    
+    $ZoneData = [ordered]@{
+        Name           = $ZoneInfo.result.name
+        SyncDate       = $Timestamp
+        CfNameServers  = $ZoneInfo.result.name_servers
+        Status         = $ZoneInfo.result.status
+        ZoneFileData   = $ZoneFileData
+        RecordsHtml    = $RecordsHtml
+        ITGOrg         = $MatchId
     }
+    $ZoneData
 }
 
 function Get-CloudflareZoneDataArray {
     $ZoneDataArray = @()
     $AllZones = New-CloudflareWebRequest -Endpoint 'zones'
+    $ITGDomains = New-ITGlueWebRequest -Endpoint 'domains' | ForEach-Object data | ForEach-Object attributes
+
     $Progress = 0
 
     foreach ($Zone in $AllZones.result) {
         Write-Progress -Activity 'CloudflareAPI' -Status 'Getting Zone Data' -CurrentOperation $Zone.name -PercentComplete ($Progress / ($AllZones.result | Measure-Object | ForEach-Object count) * 100) -Id 1
-        $ZoneData = Get-CloudflareZoneData -ZoneId $Zone.id
+        $ZoneData = Get-CloudflareZoneData -ZoneId $Zone.id -ITGDomains $ITGDomains
         if ($ZoneData) {
             $ZoneDataArray += New-Object psobject -Property $ZoneData
         }
