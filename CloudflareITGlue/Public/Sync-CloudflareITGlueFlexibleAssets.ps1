ab57026fbd329f35591f6c4bf9e5952b2bb13f7a
unknown <bdotsoul@gmail.com>
2018-11-14 23:50:11 -0800

    Formatting

--- CloudflareITGlue/Public/Sync-CloudflareITGlueFlexibleAssets.ps1
+++ CloudflareITGlue/Public/Sync-CloudflareITGlueFlexibleAssets.ps1
@@ -8,14 +8,12 @@ function Sync-CloudflareITGlueFlexibleAssets {
     $FlexAssetTypeId = New-ITGlueWebRequest -Endpoint 'flexible_asset_types' -Method 'GET' | ForEach-Object data | Where-Object {$_.attributes.name -eq $FlexAssetType} | ForEach-Object id
 
     foreach ($ZoneData in $ZoneDataArray) {
-
         Write-Progress -Activity 'ITGlueAPI' -Status 'Syncing Flexible Assets' -CurrentOperation $ZoneData.name -PercentComplete ($Progress / ($ZoneDataArray | Measure-Object | foreach-object count) * 100) -Id 2
 
         $TempFile = New-TemporaryFile
         $ZoneData.ZoneFileData | Out-file $TempFile -Force -Encoding utf8
         $Base64ZoneFile = ([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($TempFile)))
         Remove-Item $TempFile -Force
-
         $Body = @{
             data = @{
                 'type'       = 'flexible-assets'
@@ -37,10 +35,10 @@ function Sync-CloudflareITGlueFlexibleAssets {
                 }
             }
         }
-
         $Body = $Body | ConvertTo-Json -Depth 4
         $FlexAssets = New-ITGlueWebRequest -Endpoint "flexible_assets?filter[flexible_asset_type_id]=$FlexAssetTypeId&filter[organization_id]=$($ZoneData.ITGOrg)" -Method 'GET' | ForEach-Object data
         $PatchId = $null
+        
         foreach ($FlexAsset in $FlexAssets) {
             if ($FlexAsset.attributes.traits.name -eq $ZoneData.name) {
                 $PatchId = $FlexAsset.id
